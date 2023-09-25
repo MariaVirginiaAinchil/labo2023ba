a2f3c7dc8e59fe2c057095881e931c6f5d446301
MariaVirginiaAinchil <141954242+MariaVirginiaAinchil@users.noreply.github.com>
2023-09-24 23:42:37 -0300

    Update 611_CA_reparar_dataset_008_3ms.r

--- src/workflow-inicial/611_CA_reparar_dataset_008_3ms.r
+++ src/workflow-inicial/611_CA_reparar_dataset_008_3ms.r
@@ -10,7 +10,7 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "CA6110_3ms"
+PARAM$experimento <- "CA6110_008_3ms"
 PARAM$dataset <- "./datasets/competencia_2023.csv.gz"
 
 # valores posibles
